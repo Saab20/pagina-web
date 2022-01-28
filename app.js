131a8d3438260ae66fbd586e81b9f4d8569b2cb5
Sergio Almeciga <sergioalme24@hotmail.com>
2022-01-27 20:42:09 -0500

    Se adicionan codigo del proyecto

---
+++ app.js
@@ -0,0 +1,27 @@
+const express =require("express")
+const path= require("path")
+const app=express()
+
+app.get('/', (req,res)=>{
+    res.send("ahora si entre")
+})
+
+app.get('/home', (req,res)=>{
+    res.sendFile((__dirname + '/views/home.html'))
+
+})
+app.get('/login', (req,res)=>{
+    res.sendFile((__dirname + '/views/login.html'))
+
+})
+app.get('/register', (req,res)=>{
+    res.sendFile((__dirname + '/views/register.html'))
+
+})
+
+app.use(express.static(path.join(__dirname, './public')))
+
+app.listen(3003,()=>{
+    console.log("servidor corriendo")
+
+})
