443a34ccd6e9276d76646f4d2b30276c34346898
Sergio Almeciga <sergioalme24@hotmail.com>
2022-01-27 21:01:58 -0500

    Se adicionan codigo del proyecto

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.get('/register', (req,res)=>{
 
 app.use(express.static(path.join(__dirname, './public')))
 
-app.listen(3003,()=>{
+app.listen(process.env.PORT||3003,function(){
     console.log("servidor corriendo")
 
 })
